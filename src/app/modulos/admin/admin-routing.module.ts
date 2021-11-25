382bf50bf0e5ed4dc79b40fc9d0aef58e9af8629
angelicapresiga <angelicapresiga82@gmail.com>
2021-11-24 21:53:13 -0500

    enrutamiento

--- src/app/modulos/admin/admin-routing.module.ts
+++ src/app/modulos/admin/admin-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateComponent } from '../admin/usuarios/create/create.component';
+import { EditComponent } from '../admin/usuarios/edit/edit.component';
+import { GetComponent } from '../admin/usuarios/get/get.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'create',
+    component: CreateComponent,
+  },{
+    path: 'edit',
+    component: EditComponent,
+  },{
+    path: 'get',
+    component: GetComponent,
+  },{
+    path: '',
+    redirectTo: 'get'
+  }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
